8e3a6de0dd7b7199180e45b9088847fc620387df
Markd-project <markd1885929@gmail.com>
2025-03-23 23:31:35 -0400

    drawio

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,28 @@
 #13
 #14
 #FizzBuzz
+
+#runThrough = range(1, 26)
+#fBlist = (())
+#for x in runThrough:
+
+#        if x % 3 == 0:
+#            runThrough.pop(x)
+#            runThrough.insert(x, "Fuzz")
+#print(runThrough)
+
+fizzList = [x for x in range(1, 26) if x % 3 == 0]
+buzzList = [x for x in range(1, 26) if x % 5 == 0]
+
+runThrough = range(1, 26)
+
+for x in runThrough:
+    if x in (fizzList and buzzList):
+        x = "FizzBuzz"
+    if x in fizzList:
+        x = "Fizz"
+    if x in buzzList:
+        x = "Buzz"
+    else:
+        print(x)
+
